d09ab8e6c9ec4e8530209e9a78a684ecdc52a9cb
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-05 12:56:51 +0000

    ヘッダーフッター実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :installs
-  # root to: "homes#top"
+  devise_for :users
+  root "homes#top"
+  get "home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
